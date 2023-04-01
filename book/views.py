be2febb20f8c12d75b363287e45bd75401a26b3f
AswathiMohan23 <achu999here@gmail.com>
2023-04-01 15:53:21 +0530

    [ASWATHI] ADDED . get operation for cart

--- book/views.py
+++ book/views.py
@@ -64,11 +64,20 @@ class CartViews(APIView):
     def post(self, request):
         try:
             request.data.update({'user': request.user.id})
-
             serializer = CartSerializer(data=request.data)
             serializer.is_valid(raise_exception=True)
             serializer.save()
             return Response({"message": "book added to cart", "status": 200, "data": serializer.data},
+                            status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({"message": e.args[0], "status": 400, "data": {}},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request):
+        try:
+            cart_data = CartModel.objects.filter(user=request.user.id)
+            serializer = CartSerializer(cart_data, many=True)
+            return Response({"message": "cart displayed", "status": 200, "data": serializer.data},
                             status=status.HTTP_200_OK)
         except Exception as e:
             return Response({"message": e.args[0], "status": 400, "data": {}},
